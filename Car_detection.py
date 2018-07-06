bcc3f11f2be172cc364b66a5be182375dd1582a7
Akash <akash.darknight@gmail.com>
2018-07-06 13:03:00 +0530

    Script

--- Car_detection.py
+++ Car_detection.py
@@ -112,3 +112,24 @@ with tf.Session() as test_a:
     box1 = (2, 1, 4, 3)
     box2 = (1, 2, 3, 4)
     print("iou = " + str(iou(box1, box2)))
+
+
+    def yolo_non_max_suppression(scores, boxes, classes, max_boxes=10, iou_threshold=0.5):
+        """
+        Applies Non-max suppression (NMS) to set of boxes
+
+        Arguments:
+        scores -- tensor of shape (None,), output of yolo_filter_boxes()
+        boxes -- tensor of shape (None, 4), output of yolo_filter_boxes() that have been scaled to the image size (see later)
+        classes -- tensor of shape (None,), output of yolo_filter_boxes()
+        max_boxes -- integer, maximum number of predicted boxes you'd like
+        iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
+
+        Returns:
+        scores -- tensor of shape (, None), predicted score for each box
+        boxes -- tensor of shape (4, None), predicted box coordinates
+        classes -- tensor of shape (, None), predicted class for each box
+
+        Note: The "None" dimension of the output tensors has obviously to be less than max_boxes. Note also that this
+        function will transpose the shapes of scores, boxes, classes. This is made for convenience.
+        """
