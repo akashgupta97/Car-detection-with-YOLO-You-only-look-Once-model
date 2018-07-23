330a19f4bac4a02d93d47891096c1b888afa97e1
Akash <akash.darknight@gmail.com>
2018-07-23 13:03:00 +0530

    Script

--- Car_detection.py
+++ Car_detection.py
@@ -225,3 +225,7 @@ sess = K.get_session()
 class_names = read_classes("model_data/coco_classes.txt")
 anchors = read_anchors("model_data/yolo_anchors.txt")
 image_shape = (720., 1280.)
+
+
+yolo_model  = load_model("model_data/yolo.h5")
+
